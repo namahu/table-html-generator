608631d98a74cdd84c769365fd8b4aa85f404dc3
namahu <neti0326@gmail.com>
2021-09-05 10:49:44 +0900

    feat: テキストの装飾（bold, italic, underline, line-through）の再現を実装 #21

--- src/GenerateHTML.ts
+++ src/GenerateHTML.ts
@@ -1,9 +1,27 @@
 import { TextStyle } from "./SheetStyle";
 
+const generateTextDecorationLineStyle = (
+    isUnderline: boolean,
+    isStrikethrough: boolean
+) => {
+    if (isUnderline && isStrikethrough) return "underline line-through";
+    if (isUnderline) return "underline";
+    if (isStrikethrough) return "line-through";
+    return "none";
+};
+
 const generateStyle = (textStyle: TextStyle): string => {
-    return "font-size: " + textStyle.fontSize + ";"
+    return "font-size: " + textStyle.fontSize + "px;"
         + "font-family: " + textStyle.fontFamily + ";"
-        + "color: " + textStyle.foregroudColor + ";";
+        + "color: " + textStyle.foregroudColor + ";"
+        + "font-weight: " + (textStyle.isBold ? "bold" : "normal") + ";"
+        + "font-style: " + (textStyle.isItalic ? "italic" : "normal") + ";"
+        + "text-decoration-line: " + (
+            generateTextDecorationLineStyle(
+                textStyle.isUnderline,
+                textStyle.isStrikethrough
+            )
+        ) + ";";
 };
 
 const generateRowHTML = (row: any[], textStyles: TextStyle[], isHeader: boolean) => {
